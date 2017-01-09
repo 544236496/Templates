14f738a8fde70106646b2164a268bf8b8215b356
unknown <544236496@qq.com>
2017-01-09 21:23:40 +0800

    首次提交

--- app/src/main/java/code/template/com/Activity/MainActivity.java
+++ app/src/main/java/code/template/com/Activity/MainActivity.java
@@ -3,14 +3,18 @@ package code.template.com.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
+
 import code.template.com.Base.BaseActivity;
 import code.template.com.R;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "测试上传项目到github", Toast.LENGTH_SHORT).show();
     }
